dcdd924e1366343b2adc5394f60b0bb3ad0140eb
felipeLx <felipealvesfinanceiro@gmail.com>
2023-11-29 14:08:22 -0300

    :bug: find the cart issue1

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -26,13 +26,13 @@ export const meta: MetaFunction<
 export const loader = async ({request}: LoaderFunctionArgs) => {
   let initial = await loadQuery<ProductDocument[]>(PRODUCTS_QUERY).then((res) => ({
     ...res,
-    data: res.data ? productsZ.parse(res.data.map((product) => ({
+    data: res.data ? productsZ.parse(JSON.stringify(res.data.map((product) => ({
       ...product,
       stripeProductId: product.stripeProductId.slice(0, 30),
       sku: product.sku!.slice(0, 14),
       name: product.name.slice(0, 14),
       description: product.description.slice(0, 40),
-    }))) : null,
+    })))) : null,
   }))
 
   if (!initial.data) {
