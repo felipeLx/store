cf27dac957bffa05641c037e1a730d28d7984f4f
felipeLx <felipealvesfinanceiro@gmail.com>
2023-12-07 13:48:37 -0300

    :bug: footter align

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,7 +7,7 @@ export function Footer(props: LogoProps) {
   return (
     <footer className="min-h-[200px] bg-black w-full block">
       <div className="container mx-auto flex items-center justify-between px-4 lg:px-12">
-        <div className="flex mt-2 max-w-sm text-right flex-row items-start justify-start gap-2 text-sm lg:flex-row lg:items-center lg:gap-5">
+        <div className="flex mt-2 max-w-sm text-right flex-row items-start justify-start gap-2 text-sm lg:flex-row lg:items-center lg:gap-5 md:flex-col sm:flex-col">
           <ul className='flex flex-row align-baseline text-inherit gap-2'>
           <li className="text-white mr-4 text-sm">
               <Link to="/">Home</Link></li>
@@ -17,7 +17,7 @@ export function Footer(props: LogoProps) {
             <li className="text-white mr-4 text-sm"><Link to="tos">Termo de Serviço</Link></li>
           </ul>
         </div>
-        <div className="flex max-w-sm text-right flex-1 flex-col items-end justify-end gap-2 text-sm lg:flex-row lg:items-center lg:gap-5">
+        <div className="flex max-w-sm text-right flex-1 flex-col items-end justify-end gap-2 text-sm lg:flex-row lg:items-center lg:gap-5 md:flex-col sm:flex-col">
           <ul className='flex flex-row align-baseline text-inherit gap-2'>
             <li className="text-white mr-4 text-sm">
               <a target='_blank' rel='noreferrer' href='https://www.facebook.com/artesanatosdazizies'><Facebook /></a></li>
