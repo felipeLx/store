e6d693bcfd69129890a549e46a8efd27ab4b9d56
felipeLx <felipealvesfinanceiro@gmail.com>
2023-12-24 15:54:15 -0300

    add more text on layout

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -11,7 +11,13 @@ export function Layout({home, children}: PropsWithChildren<LogoProps>) {
     <>
       <Header />
       <div className="flex flex-col w-fill mb-2 pb-2">
-        {home?.title ? <Title data={home} /> : null}
+        <div className='p-2'>
+          {home?.title ? <Title data={home} /> : null}
+          <h6>
+            <span className="text-gray-400">Artes prontas e únicas para venda</span>
+          </h6>
+          <p>Diferente de outros sites. no Artesanatos da Zizi você encontra produtos prontos, realizados para exposição e feiras, e agora disponíveis para a venda. Produto único, não há reprodução.</p>
+        </div>
         {children}
         <ShoppingCartModal />
       </div>
